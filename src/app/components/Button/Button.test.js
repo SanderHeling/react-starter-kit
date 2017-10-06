4c81ce487ebd71cce843d52286cb5d47224d3c9f
Sander Heling <sander@wend.nl>
2017-10-06 07:04:12 +0200

    Remove Button test
    The snapshot test for button are already run by the storybook tests.
    So we can just remove it for now.

--- src/app/components/Button/Button.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import Button from './Button';
-
-test('should render correctly', () => {
-    const component = renderer.create(<Button>Text</Button>).toJSON();
-    expect(component).toMatchSnapshot();
-});
