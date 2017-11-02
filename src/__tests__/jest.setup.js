afad308887f684266168c8eb2a2b8085fe4024e5
Sander Heling <sander@wend.nl>
2017-11-02 06:54:02 +0100

    Fix tests for React 16
    Added enzyme-adapter-react-16 to let the tests work again

--- src/__tests__/jest.setup.js
+++ src/__tests__/jest.setup.js
@@ -1,3 +1,9 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// Configure Enzyme for React 16
+Enzyme.configure({ adapter: new Adapter() });
+
 // Throw console.errors (like prop validation) to Errors so the test fail.
 const spy = jest.spyOn(console, 'error').mockImplementation(error => {
     throw new Error(error);
